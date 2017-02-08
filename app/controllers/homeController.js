0c73a6af985370b422ce15fff4bc85ee70431ef0
simon245 <simonball245@gmail.com>
2017-02-08 16:35:09 +0000

    Added basic pig latin conversion

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,5 +1,30 @@
 angular.module('pigLatinApp').controller('HomeController', function ($scope) {
   var self = this;
 
-  this.message = 'Test message';
+  this.inputField = "";
+
+  this.outputField = "";
+
+  this.makePigLatin = function(){
+    var string = self.inputField.toLowerCase();
+    var firstLetter = string.slice(0, 1);
+    var remainingChar = string.substr(1);
+    var vowels = ['a', 'e', 'i', 'u'];
+    var isVowel = false;
+
+    if(string === ""){
+      self.outputField = "";
+      return;
+    }
+
+    _.each(vowels, function(v){
+      if(firstLetter === v){
+        self.outputField = remainingChar + firstLetter + "i";
+        return false;
+      }else{
+        self.outputField = remainingChar + firstLetter + "ay";
+      }
+    });
+  };
+
 });
