686c6850a60d0d339b396900d8a2615d0160cc51
simon245 <simonball245@gmail.com>
2017-02-09 11:14:35 +0000

    added start to scripts in package.json

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express  = require('express');
 var app      = express();
 var port     = process.env.PORT || 8080;
-
-
 app.use(express.static(__dirname + '/app'));
 
 require('./routes.js')(app);
